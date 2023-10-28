7b6a73e1016140550492bf35b8e28a4fc97bc307
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-28 12:55:19 -0600

    --Start: off building out "AddExpenseView"

---
+++ ExpenseTrackerPractice2/View/AddExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpenseView.swift
+//  ExpenseTrackerPractice2
+//
+//  Created by Mitch Andrade on 10/28/23.
+//
+
+import SwiftUI
+
+struct AddExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddExpenseView()
+}
