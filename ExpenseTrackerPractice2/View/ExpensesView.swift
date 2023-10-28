49aefa4e6856cc93778f3004d5c1cc33b8358269
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-28 11:04:47 -0600

    --Progress: -- building out the main Views for Expenses and Categories

---
+++ ExpenseTrackerPractice2/View/ExpensesView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpensesView.swift
+//  ExpenseTrackerPractice2
+//
+//  Created by Mitch Andrade on 10/28/23.
+//
+
+import SwiftUI
+
+struct ExpensesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpensesView()
+}
