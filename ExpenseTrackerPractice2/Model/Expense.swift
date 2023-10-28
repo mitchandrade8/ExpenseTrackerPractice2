67c3d9d9af2cd2568d3a98411fba7dbde557d7e1
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-28 10:44:20 -0600

    --Updating: to model setup for Expense Tracker app

--- ExpenseTrackerPractice2/Model/Expense.swift
+++ ExpenseTrackerPractice2/Model/Expense.swift
@@ -5,4 +5,21 @@
 //  Created by Mitch Andrade on 10/28/23.
 //
 
-import Foundation
+import SwiftUI
+import SwiftData
+
+@Model
+class Expense {
+    /// Expense Properties
+    var title: String
+    var subTitle: String
+    var amount: Double
+    var date: Date
+    
+    init(title: String, subTitle: String, amount: Double, date: Date) {
+        self.title = title
+        self.subTitle = subTitle
+        self.amount = amount
+        self.date = date
+    }
+}
