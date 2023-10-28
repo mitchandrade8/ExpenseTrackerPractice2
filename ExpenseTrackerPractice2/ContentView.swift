ac106bb1a4ce4c737ce87c212400c4f891ddf458
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-27 18:51:47 -0600

    Initial Commit

---
+++ ExpenseTrackerPractice2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseTrackerPractice2
+//
+//  Created by Mitch Andrade on 10/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpenseTrackerPractice2/ExpenseTrackerPractice2App.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerPractice2App.swift
+//  ExpenseTrackerPractice2
+//
+//  Created by Mitch Andrade on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerPractice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
