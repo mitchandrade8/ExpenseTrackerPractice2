44c679612704353ef0d6c3cd6a81d335d81d5e9b
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-28 11:40:38 -0600

    --Progress: Added a tabview and "@State" property to track what tab the user is on

---
+++ ExpenseTrackerPractice2/View/CategoriesView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesView.swift
+//  ExpenseTrackerPractice2
+//
+//  Created by Mitch Andrade on 10/28/23.
+//
+
+import SwiftUI
+
+struct CategoriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoriesView()
+}
