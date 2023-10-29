6b52820061034c69015d2d7baf9c6759bfa74b41
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-28 18:19:57 -0600

    --Progress: now displaying today CardViews of Expense and NO ERRORS

---
+++ ExpenseTrackerPractice2/View/ExpenseCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseCardView.swift
+//  ExpenseTrackerPractice2
+//
+//  Created by Mitch Andrade on 10/28/23.
+//
+
+import SwiftUI
+
+struct ExpenseCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseCardView()
+}
